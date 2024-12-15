0e877f89d5146e8cdfd1065898e04e8db74b93e8
vrajeshhb <67744630+vrajeshhb@users.noreply.github.com>
2024-12-15 11:12:06 +0530

    Update TaskTable.js

--- src/comp/TaskTable.js
+++ src/comp/TaskTable.js
@@ -44,7 +44,7 @@ const TaskTable = () => {
   };
   const fetchTasks = async () => {
     try {
-      const response = await fetch(`${process.env.REACT_APP_API_KEY}`);
+      const response = await fetch(`https://jsonplaceholder.typicode.com/todos`);
       if (!response.ok) {
         throw new Error("Failed to fetch tasks");
       }
